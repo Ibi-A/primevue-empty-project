10f10916a78ca80581dd22d5086d3e037ff026bd
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-08 20:41:05 +0100

    initialized empty project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
 
-createApp(App).use(router).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';    
+
+const app = createApp(App)
+
+app.use(PrimeVue)
+app.use(router)
+
+app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="home">
-
-  </div>
+    <Button label="Button"/>
 </template>
 
 <script>
+import Button from 'primevue/button'
 
 export default {
   components: {
-    HelloWorld
+    Button,
   }
 }
 </script>
